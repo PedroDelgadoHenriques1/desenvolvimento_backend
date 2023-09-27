acae8e0ec299425f7aa87914d30ff5cf0b037be6
Pedro Delgado Henriques <pedrodelgadohenriques1@gmail.com>
2023-09-26 22:06:38 -0300

    fazendo personalização de django admin

--- usipav/admin.py
+++ usipav/admin.py
@@ -5,3 +5,16 @@ admin.site.register(Topico)
 admin.site.register(Pagina)
 admin.site.register(RegistroAcesso)
 admin.site.register(Pessoa)
+
+# Classe de personalização do modelo
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'cpf', 'verify_nome')
+    list_filter = ('cpf',)
+    search_fields = ('nome', 'sobrenome', 'cpf')
+    ordering = ('nome',)
+
+# Desregistra o modelo existente
+admin.site.unregister(Pessoa)
+
+# Registra o modelo com a personalização
+admin.site.register(Pessoa, PessoaAdmin)
